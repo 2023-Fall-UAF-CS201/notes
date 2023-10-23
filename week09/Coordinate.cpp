5bd4fe09f1239f37ba5a60dc2c42afba90927acd
Lane Schwartz <dowobeha@gmail.com>
2023-10-23 10:16:56 -0800

    Demo code

---
+++ week09/Coordinate.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class Coordinate {
+  double x;
+  double y;
+};
+
+
+
+int main() {
+
+  Coordinate joe;
+
+  cout << joe.x << endl;
+  
+  return 0;
+}
+
