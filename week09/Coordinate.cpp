69efd1b9451434f4aab6c7db82c81c28cac65a79
Lane Schwartz <dowobeha@gmail.com>
2023-10-27 09:14:40 -0800

    CoordinatE'

--- week09/Coordinate.cpp
+++ week09/Coordinate.cpp
@@ -3,18 +3,36 @@
 using std::cout;
 using std::endl;
 
-class Coordinate {
+class Coordinate {   // Define a custom data type called Coordinate
+
+  // Defined *member variables*
   double x;
   double y;
-};
+
+public:
+  
+  double getX() {
+    return x;
+  }
+
+  void setX(double newX) {
+    this->x = newX;
+  }
+  
+};  // NOTE: semicolon is needed
 
 
 
 int main() {
 
   Coordinate joe;
+  joe.setX(15.0);
 
-  cout << joe.x << endl;
+  Coordinate pi;
+  pi.setX(3.14);
+  
+  cout << joe.getX() << endl;
+  cout << pi.getX() << endl;
   
   return 0;
 }
