ab13daea45e2ab5e47e1c85875e4f3d4d9d2920c
Lane Schwartz <dowobeha@gmail.com>
2023-11-13 11:25:28 -0900

    Enum

---
+++ week12/Seasons.h
@@ -0,0 +1,22 @@
+enum class Season { earlyWinter, // October
+		     winter,     // November, February
+		     deepWinter, // December, January
+		     lateWinter, // March
+		     fakeSpring, // April
+		     spring,     // May
+		     summer,     // June, July
+		     fall        // August, September
+		     };
+
+
+
+  auto season = Season::winter;
+
+  switch(season) {
+  case Season::spring: cout << "Yaaaayyyy"; break;
+  case Season::summer: cout << "Go hiking"; break;
+    //  default:             cout << "Ho hum"; break;
+  }
+
+int main() {
+}
